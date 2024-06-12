84d81c2ecafa44491fc833e1a16de7513d3fe759
Simon Li <simonjunyou.li@gmail.com>
2024-06-12 19:58:30 -0400

    test 3 categories

--- bootstrap_test.sql
+++ bootstrap_test.sql
@@ -86,12 +86,20 @@ CREATE TABLE HelloWorld (
 -- Insert one row with the value 'World'
 INSERT INTO HelloWorld (Hello) VALUES ('World');
 
--- Test Queries
+-- Test Popularity Queries
 
 INSERT INTO Album (AlbumID, Name) VALUES (1, 'Album1');
 INSERT INTO Singer (SingerID, Name, Age, SongProduced, Country) VALUES (1, 'Teacher G', 30, 10, 'USA');
 
 INSERT INTO Song (SongID, SingerID, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
     (1, 1, '2021-01-01', 'Pop', 0, 100, 50, 'spotify.com', 'youtube.com', 1);
+INSERT INTO Song (SongID, SingerID, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
+    (2, 1, '2021-01-02', 'Pop', 0, 200, 100, 'spotify.com', 'youtube.com', 1);
+INSERT INTO Song (SongID, SingerID, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
+    (3, 1, '2021-01-03', 'Pop', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
+INSERT INTO Song (SongID, SingerID, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
+    (4, 1, '2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
+INSERT INTO Song (SongID, SingerID, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
+    (5, 1, '2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
 
-SELECT (Liked + Disliked) AS TotalLikes FROM Song WHERE SongID = 1;
+SELECT Category, COUNT(*) AS NumberOfSongs FROM Song WHERE SingerID = 1 GROUP BY Category ORDER BY NumberOfSongs DESC LIMIT 3;
