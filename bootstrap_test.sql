28e20184578a3c1375a3b2a5727c8891b9208442
Sparky Fnay <Sparky@v1040-wn-rt-a-2-145.campus-dynamic.uwaterloo.ca>
2024-06-19 17:45:12 -0400

    huh

--- bootstrap_test.sql
+++ bootstrap_test.sql
@@ -24,20 +24,20 @@ INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReview
     (1, 1, "AAA", '2021-01-01', 'Pop', 0, 100, 50, 'spotify.com', 'youtube.com', 1);
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
     (2, 2, "BBB", '2021-01-02', 'Pop', 0, 200, 100, 'spotify.com', 'youtube.com', 1);
-INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
-    (3, 2, "CCC", '2021-01-03', 'Pop', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
-INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
-    (4, 2, "DDD",'2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
-INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
-    (5, 2, "EEE",'2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
+-- INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
+--     (3, 2, "CCC", '2021-01-03', 'Pop', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
+-- INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
+--     (4, 2, "DDD",'2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
+-- INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
+--     (5, 2, "EEE",'2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
 
-INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
-    ('2021-01-01', 1, 1, TRUE, 'Great Teacher Tou very epic');
-INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
-    ('2021-01-03', 1, 2, TRUE, 'Teacher G is good');
-INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
-    ('2021-01-02', 2, 1, TRUE, 'Good Teacher G epic');
-INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
-    ('2021-01-02', 3, 1, FALSE, 'Good Teacher G epic');
+-- INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
+--     ('2021-01-01', 1, 1, TRUE, 'Great Teacher Tou very epic');
+-- INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
+--     ('2021-01-03', 1, 2, TRUE, 'Teacher G is good');
+-- INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
+--     ('2021-01-02', 2, 1, TRUE, 'Good Teacher G epic');
+-- INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
+--     ('2021-01-02', 3, 1, FALSE, 'Good Teacher G epic');
 
 SELECT * FROM SONG WHERE SongID = 1;
