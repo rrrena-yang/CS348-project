6be9db3371966a2a4a5133fa6eceae44744d2dd4
Sparky Fnay <Sparky@v1040-wn-rt-a-2-145.campus-dynamic.uwaterloo.ca>
2024-06-19 17:49:13 -0400

    asd

--- bootstrap_test.sql
+++ bootstrap_test.sql
@@ -25,11 +25,11 @@ INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReview
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
     (2, 2, "AAA", '2021-01-02', 'Pop', 0, 200, 100, 'spotify.com', 'youtube.com', 1);
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
-    (3, 3, "CCC", '2021-01-03', 'Pop', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
+    (3, 2, "CCC", '2021-01-03', 'Pop', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
-    (4, 4, "DDD", '2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
+    (4, 2, "DDD", '2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
-    (5, 5, "EEE",'2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
+    (5, 2, "EEE",'2021-01-03', 'Rock', 0, 300, 150, 'spotify.com', 'youtube.com', 1);
 
 INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
     ('2021-01-01', 1, 1, TRUE, 'Great Teacher Tou very epic');
