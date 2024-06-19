5b481938f2e7469f4347d9556d6d2ab23a4bb88a
Sparky Fnay <Sparky@v1040-wn-rt-a-2-145.campus-dynamic.uwaterloo.ca>
2024-06-19 19:31:03 -0400

    asd

--- sample-query/pop_song_between_2010_2000/basic.sql
+++
@@ -1,7 +0,0 @@
-SELECT SongName, COUNT(*)
-FROM Song
-    JOIN UserReviewOnSong ON Song.SongID = UserReviewOnSong.SongID
-    JOIN User ON UserReviewOnSong.UserID = User.ID
-WHERE BirthYear >= 2000
-    AND BirthYear <= 2010
-    GROUP BY SongName;
