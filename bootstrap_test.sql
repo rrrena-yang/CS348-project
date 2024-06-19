841c8368f2f470eec7234c3e87624366ed51b407
Simon Li <simonjunyou.li@gmail.com>
2024-06-19 19:11:17 -0400

    add userpreference

--- bootstrap_test.sql
+++ bootstrap_test.sql
@@ -10,15 +10,10 @@ INSERT INTO HelloWorld (Hello) VALUES ('World');
 INSERT INTO Album (AlbumID, Name) VALUES (1, 'Album1');
 INSERT INTO Album (AlbumID, Name) VALUES (2, 'Album2');
 
-<<<<<<< HEAD
-INSERT INTO Singer (SingerID, Name, Age, SongProduced, Country) VALUES (1, 'Teacher To', 30, 10, 'USA');
-INSERT INTO Singer (SingerID, Name, Age, SongProduced, Country) VALUES (2, 'Teacher G', 30, 10, 'USA');
-INSERT INTO Singer (SingerID, Name, Age, SongProduced, Country) VALUES (3, 'Teacher C', 30, 10, 'USA');
-
-=======
-INSERT INTO Singer (SingerID, Name, BirthYear, SongProduced, Country) VALUES (1, 'Teacher Tou', 30, 10, 'USA');
+INSERT INTO Singer (SingerID, Name, BirthYear, SongProduced, Country) VALUES (1, 'Teacher To', 30, 10, 'USA');
 INSERT INTO Singer (SingerID, Name, BirthYear, SongProduced, Country) VALUES (2, 'Teacher G', 30, 10, 'USA');
->>>>>>> 5764398199a08c68e76e25132f74ef05fd21e5c3
+INSERT INTO Singer (SingerID, Name, BirthYear, SongProduced, Country) VALUES (3, 'Teacher C', 30, 10, 'USA');
+
 
 INSERT INTO User (ID, UserPassword, UserName, Name, BirthYear)  VALUES 
     (1, 'password', 'username', 'name', 1999);
@@ -30,7 +25,7 @@ INSERT INTO User (ID, UserPassword, UserName, Name, BirthYear)  VALUES
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
     (1, 1, "AAA", '2021-01-01', 'Pop', 0, 100, 50, 'spotify.com', 'youtube.com', 1);
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
-    (2, 2, "AAA", '2021-01-02', 'Pop', 0, 200, 100, 'spotify.com', 'youtube.com', 1);
+    (2, 1, "BBB", '2021-01-02', 'Pop', 0, 200, 100, 'spotify.com', 'youtube.com', 1);
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
     (3, 2, "CCC", '2021-01-03', 'Pop', 0, 300, 150, 'spotify.com', 'youtube.com', 2);
 INSERT INTO Song (SongID, SingerID, SongName, PublishDate, Category, TotalReviewAmount, Liked, Disliked, SpotifyLink, YTLink, AlbumID) VALUES 
@@ -46,7 +41,7 @@ INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES
 INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
     ('2021-01-03', 1, 2, TRUE, 'Teacher G is good');
 INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
-    ('2021-01-02', 2, 1, TRUE, 'Good Teacher G epic');
+    ('2021-01-02', 2, 5, TRUE, 'Good Teacher G epic');
 INSERT INTO UserReviewOnSong (Timestamp, UserID, SongID, IsLike, Review) VALUES 
     ('2021-01-02', 3, 1, FALSE, 'Good Teacher G epic');
 
@@ -68,69 +63,3 @@ WHERE
     urs.IsLike = TRUE
 GROUP BY 
     urs.UserID, s.Category, s.AlbumID, s.SingerID;
-
-
-SELECT 
-    urs.UserID,
-    urs.Category,
-    urs.AlbumID,
-    urs.SingerID,
-    urs.LikeCount
-FROM 
-    UserPreferences urs
-WHERE 
-    urs.UserID = 1
-ORDER BY 
-    LikeCount DESC
-LIMIT 3;
-
-WITH UserProfile(UserID, Category, AlbumID, SingerID, LikeCount) AS (
-    SELECT 
-        urs.UserID,
-        urs.Category,
-        urs.AlbumID,
-        urs.SingerID,
-        urs.LikeCount
-    FROM 
-        UserPreferences urs
-    WHERE 
-        urs.UserID = 1
-    ORDER BY 
-        LikeCount DESC
-    LIMIT 3
-)
-SELECT 
-    s.SongID,
-    s.SongName,
-    s.Category,
-    s.AlbumID,
-    s.SingerID
-FROM 
-    Song s
-WHERE 
-    s.Category IN (SELECT Category FROM UserProfile)
-    OR s.AlbumID IN (SELECT AlbumID FROM UserProfile)
-    OR s.SingerID IN (SELECT SingerID FROM UserProfile)
-ORDER BY 
-    RAND()
-LIMIT 10;
-
-
--- SELECT 
---     s.SongID,
---     s.SongName,
---     s.Category,
---     s.AlbumID,
---     s.SingerID
--- FROM 
---     Song s
--- WHERE 
---     s.Category IN (SELECT Category FROM UserPreferences WHERE UserID = 2)
---     OR s.AlbumID IN (SELECT AlbumID FROM UserPreferences WHERE UserID = 2)
---     OR s.SingerID IN (SELECT SingerID FROM UserPreferences WHERE UserID = 2)
--- ORDER BY 
---     RAND()
--- LIMIT 10;
-
-
-SELECT * FROM Song WHERE SongID = 1;
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ find sample-query -type f -name '*.sql' | while read -r sql_file; do
     out_file="${sql_file%.sql}.out"
     output=$(mysql -uroot -p"MySQL030927" CS348_TEST < "$sql_file")
     echo "$output" > output.txt
-    if ! diff -q output.txt "$out_file"; then
+    if ! diff -w -q output.txt "$out_file"; then
     echo "Output mismatch for $sql_file"
     echo "===== Expected Output ($out_file) ====="
     cat "$out_file"
