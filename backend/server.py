b7fe48c195d83ca208d464346140c97a08ce6945
Ziheng Zhou <zihengzhou@v1040-wn-rt-b-92-238.campus-dynamic.uwaterloo.ca>
2024-07-01 16:07:27 -0400

    added roupte for popular_song_by_age

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,11 @@
 from SQLService import get_connector
 from flask import Flask, jsonify, request, render_template, redirect, url_for
+from r9_popular_song_age_group import r9
 
 app = Flask(__name__, template_folder='../templates/rock', static_folder='../static')
 
+app.register_blueprint(r9)
 conn = get_connector()
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
