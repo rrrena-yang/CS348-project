6afce687618ac5a04d97bfed03efcc0d679b5c2d
Zora Liu <z25liu@uwaterloo.ca>
2024-07-08 21:55:25 -0400

    add

--- backend/r7_user_recommendation.py
+++ backend/r7_user_recommendation.py
@@ -9,7 +9,36 @@ def user_recommendation(user_id):
     file_path = '../sample-query/user_recommendation/user_rec.txt'
     with open(file_path, 'r') as file:
         file_content = file.read()
-    query = eval('f' + repr(file_content))
+    query = f"""
+        WITH UserProfile(UserID, Category, AlbumID, SingerID, LikeCount) AS (
+    SELECT 
+        urs.UserID,
+        urs.Category,
+        urs.AlbumID,
+        urs.SingerID,
+        urs.LikeCount
+    FROM 
+        UserPreferences urs
+    WHERE 
+        urs.UserID = {user_id}
+    ORDER BY 
+        LikeCount DESC
+    LIMIT 3
+)
+SELECT 
+    s.SongID,
+    s.SongName,
+    s.Category,
+    s.AlbumID,
+    s.SingerID
+FROM 
+    Song s
+WHERE 
+    s.Category IN (SELECT Category FROM UserProfile)
+    OR s.AlbumID IN (SELECT AlbumID FROM UserProfile)
+    OR s.SingerID IN (SELECT SingerID FROM UserProfile)
+LIMIT 10;
+        """
     conn = get_connector()
     cursor = conn.cursor()
     cursor.execute(query)
--- backend/r9_popular_song_age_group.py
+++ backend/r9_popular_song_age_group.py
@@ -6,45 +6,51 @@ r9 = Blueprint('r9', __name__)
 
 @r9.route('/api/popular_song_age_group', methods=['POST','GET'])
 def popular_song_age_group():
-    # data payload is in JSON format {startYear: int, endYear: int}
-    data = request.get_json()
-    startYear = data.get('startYear', 0)
-    endYear = data.get('endYear', 2999)
-    # Query to get the most popular song for each age group
+    songs = None
+    start_year = None
+    end_year = None
+    if request.method == 'POST':
+        start_year = request.form.get('startYear', 0)
+        end_year = request.form.get('endYear', 2999)
+        
+        query = f"""
+        WITH Likes AS (
+            SELECT s.SongName, s.SongID
+            FROM Song s
+                JOIN UserReviewOnSong ur ON s.SongID = ur.SongID
+                JOIN User u ON ur.UserID = u.ID
+            WHERE u.BirthYear >= {start_year}
+                AND u.BirthYear <= {end_year}
+                AND ur.IsLike = TRUE
+        ),
+        Dislikes AS (
+            SELECT s.SongName, s.SongID
+            FROM Song s
+                JOIN UserReviewOnSong ur ON s.SongID = ur.SongID
+                JOIN User u ON ur.UserID = u.ID
+            WHERE u.BirthYear >= {start_year}
+                AND u.BirthYear <= {end_year}
+                AND ur.IsLike = FALSE
+        ),
+        Diff AS (
+            SELECT l.SongName, l.SongID
+            FROM Likes l
+            EXCEPT ALL
+            SELECT d.SongName, d.SongID
+            FROM Dislikes d
+        )
+        SELECT d.SongName, d.SongID,
+            COUNT(*) AS Popularity
+        FROM Diff d
+        GROUP BY d.SongName, d.SongID;
+        """
 
-    query = f"""
-WITH Likes AS (
-SELECT *
-FROM Song
-    JOIN UserReviewOnSong ON Song.SongID = UserReviewOnSong.SongID
-    JOIN User ON UserReviewOnSong.UserID = User.ID
-WHERE BirthYear >= {startYear}
-    AND BirthYear <= {endYear}
-    AND IsLike = TRUE
-),
-Dislikes AS (
-    SELECT SongName
-    FROM Song
-        JOIN UserReviewOnSong ON Song.SongID = UserReviewOnSong.SongID
-        JOIN User ON UserReviewOnSong.UserID = User.ID
-    WHERE BirthYear >= {startYear}
-        AND BirthYear <= {endYear}
-        AND IsLike = FALSE
-),
-Diff AS (
-    SELECT SongName
-    FROM Likes
-    EXCEPT ALL
-    SELECT *
-    FROM Dislikes
-)
-SELECT SongName,
-    COUNT(*)
-FROM Diff
-GROUP BY SongName;"""
-    
-    conn = get_connector()
-    cursor = conn.cursor()
-    cursor.execute(query, (startYear, endYear, startYear, endYear))
-    songs = cursor.fetchall()
-    return render_template('user_recommendation.html', songs=songs)
+        conn = get_connector()
+        cursor = conn.cursor()
+        cursor.execute(query)
+        songs = cursor.fetchall()
+        conn.close()
+        songs = [{"songname": song[0], "song_id": song[1], "popular": song[2]} for song in songs]
+
+    return render_template('popular_songs.html', songs=songs, start_year=start_year, end_year=end_year)
+
